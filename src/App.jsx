ebba71a2a40ffa500993c00fd43807ddf8dd537a
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-01 01:35:22 -0500

    imported components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import "./App.css";
+import AllPlayers from "./components/AllPlayers";
+import NewPlayers from "./components/NewPlayerForm";
+import SinglePlayer from "./components/SinglePlayer";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <AllPlayers />
+      {/* <NewPlayers />
+    <SinglePlayer /> */}
+    </div>
+  );
+}
 
 export default App;
