3f8c9fb8fcaa4bbe73b4b80fed7378746925493e
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-02 20:00:29 -0500

    added singlePlayerList API call

--- src/API/index.js
+++ src/API/index.js
@@ -1,13 +1,70 @@
+// all players
 export const apiFetchAllPlayers = async () => {
   try {
     const response = await fetch(
       "https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-CT-WEB-PT-A/players"
     );
     const result = await response.json();
-    console.log(result);
+    // console.log(result);
     return result.data.players;
+  } catch (err) {
+    // console.error(err);
+    throw new Error(err);
+  }
+};
+
+// select single player
+
+export const singlePlayerList = async (id) => {
+  try {
+    const response = await fetch(
+      `https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-CT-WEB-PT-A/players/${id}`
+    );
+    const result = await response.json();
+    console.log(result);
+    return result.data.player;
   } catch (err) {
     console.error(err);
     throw new Error(err);
+  }
+};
+
+// add a new player
+
+// export const addNewPlayer = async () => {
+//   try {
+//     const response = await fetch(
+//       "https://fsa-puppy-bowl.herokuapp.com/api/2302-ACC-CT-WEB-PT-A/players",
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ name: name }),
+//       }
+//     );
+//     const result = await response.json();
+//     console.log(result);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// Delete Player:
+
+export const deletePlayer = async (playerId) => {
+  try {
+    const response = await fetch(
+      `https://fsa-puppy-bowl.herokuapp.com/api/COHORT-NAME/players/${playerId}`,
+      {
+        method: "DELETE",
+      }
+    );
+    const result = await response.json();
+    console.log(result);
+    apiFetchAllPlayers();
+    window.location.reload();
+  } catch (err) {
+    console.log(`Trouble removing ${playerId}`);
   }
 };
